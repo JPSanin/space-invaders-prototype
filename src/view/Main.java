03cc85278b632216523938056c53e39a1617eac4
Juan Sanin <juanpablosaninb@gmail.com>
2020-11-27 18:25:25 -0500

    First Commit

---
+++ src/view/Main.java
@@ -0,0 +1,25 @@
+package view;
+
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main(Main.class.getName());
+	}
+
+	public void settings() {
+		size(800,600);
+
+	}
+
+	public void setup() {
+	
+	}
+
+	public void draw() {
+	background(0);
+	}
+
+}
